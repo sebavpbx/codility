72558e239f53cbb47b522a8e005015b320647d67
Sebastian <seba.vpbx@gmail.com>
2019-01-14 09:02:51 +0100

    100% Verified Solution

---
+++ BinaryGap.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python2.7
+
+def solution(A):
+
+    numbers = str(bin(A))[2:]
+    lenght = len(numbers)
+    maxgap = lenght - 2
+
+    if lenght > 2 and '0' in numbers and '1' in numbers:
+        while maxgap > 0:
+            if ('1' + maxgap * '0' + '1') in numbers:
+                return maxgap
+            else:
+                maxgap -= 1
+        return 0
+    else:
+        return 0
