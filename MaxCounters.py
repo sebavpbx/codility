63d0306f47d3c0a36b1cdce508df956c994f7c20
Sebastian Bartel <seba.vpbx@gmail.com>
2019-02-16 14:24:23 +0100

    MaxCounters.py improved - verified 88% solution

--- MaxCounters.py
+++ MaxCounters.py
@@ -2,10 +2,14 @@
 
 def solution(N, A):
     counters = [0]*N
+    maximum = 0
     for element in A:
         if element > N:
-            maximum = max(counters)
             counters = [maximum] * N
         else:
             counters[element - 1] += 1
+            if counters[element - 1] > maximum:
+                maximum = counters[element - 1]
     return counters
+
+print(solution(5, [1,2,3,1,6,6]))
