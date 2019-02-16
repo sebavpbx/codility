a8f630a0d6eb1e0f26650cf4255ad1436de36895
Sebastian Bartel <seba.vpbx@gmail.com>
2019-02-16 07:07:50 +0100

    CountDiv.py - verified solution 100%

--- CountDiv.py
+++ CountDiv.py
@@ -2,10 +2,10 @@
 
 def solution(A, B, K):
 
+    addition = 0
+
     if A == 0:
         addition = 1
-    else:
-        addition = 0
 
     difference = B - A
 
@@ -15,7 +15,7 @@ def solution(A, B, K):
         else:
             return 0
     elif K == B or ((K == A) and (K == B)):
-        return 1
+        return 1 + addition
     else:
         if difference == 0:
             if A % K == 0:
@@ -42,3 +42,17 @@ def solution(A, B, K):
                         return int(difference / K) + addition
             
 print(solution(1, 1, 1))
+print(solution(0,1,1))
+print(solution(1,1,1))
+print(solution(0,2,2))
+print(solution(0,2,3))
+print(solution(2,2,1))
+print(solution(9,9,1))
+print(solution(1,9,1))
+print(solution(1,9,2))
+print(solution(2,10,2))
+print(solution(2,9,1))
+print(solution(3,9,3))
+print(solution(4,10,3))
+print(solution(5,11,3))
+print(solution(3,9,2))
