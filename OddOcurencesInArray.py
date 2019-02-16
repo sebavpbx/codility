b1ae22860630b97c16bd4082ca26d64b80c288bd
Sebastian Bartel <seba.vpbx@gmail.com>
2019-02-16 13:57:07 +0100

    OddOcurencesInArray improved - verified 100%

--- OddOcurencesInArray.py
+++ OddOcurencesInArray.py
@@ -1,9 +1,12 @@
 
-
 def solution(A):
-    checked = []
-    for number in A:
-        if number not in checked:
-            checked.append(number)
-            if A.count(number) % 2 != 0:
-                return number
+    A.sort()
+    if len(A) > 1:
+        for i in range(0, len(A), 2):
+            if i == len(A) - 1:
+                return A[i]
+            elif A[i] != A[i + 1]:
+                return A[i]
+    else:
+        return A[0]
+
