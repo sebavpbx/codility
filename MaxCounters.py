5810621664fd0aeaa5f5b34259cdc5547eef53df
Sebastian Bartel <seba.vpbx@gmail.com>
2019-02-19 12:41:12 +0100

    improved MaxCounters

--- MaxCounters.py
+++ MaxCounters.py
@@ -14,6 +14,16 @@ def solution(N, A):
 
 def solution1(N, A):
     counters = [0] * N
+    max_no = A.count(N + 1)
+    if max_no == 0:
+        for licznik in range(N):
+            counters[licznik] = A.count(licznik + 1)
+    else:
+        pass
+    return counters
     
+# lista = [1,2,3,1,6,4,4,4,3,2,1,2,3,3,3,6,1,2,1,1,3,4,5,6,2,3,4,3,3,3,5,6,6,1,2,2,2,3,4,5,5,5,5,6,1,2,3,2,1,1,6,1]
+lista = [1,2,3,1,2,4,4,4,3,2,1,2,3,3,3,1,1,2,1,1,3,4,5,5,2,3,4,3,3,3,5,4,3,1,2,2,2,3,4,5,5,5,5,2,1,2,3,2,1,1,1,1]
 
-print(solution(5, [1,2,3,1,6,6,1,2,1,1,3,4,5,6,6,1,2,2,2,3,4,5,5,5,5,6,6,1]))
+print(solution(5, lista))
+print(solution1(5, lista))
